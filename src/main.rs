def77a10f110ba653514b6f026f0f21df69c644a
Atul-Bhatt <atulbhatt61@gmail.com>
2023-02-20 16:33:15 +0530

    Multithreaded server finished.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
+use rust_web_server::ThreadPool;
 use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream}, fs,
-    thread,
-    time::Duration,
+    thread::{self, JoinHandle},
+    time::Duration, sync::Arc,
 };
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
